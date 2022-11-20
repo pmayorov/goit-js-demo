344bd04d93177a66979ba5144b6b07f68474ebdd
Pavlo Maiorov <pmayorov@gmail.com>
2022-11-20 18:49:21 +0200

    Task 31-th - OK!

---
+++ scripts/task-31.js
@@ -0,0 +1,22 @@
+/* ЗАВДАННЯ #31
+
+Доповни код, присвоївши оголошеним змінним вирази звертання до відповідних елементів або властивостей рядка у змінній course.
+
+courseTopicLength - довжина рядка.
+firstElement - перший символ рядка.
+lastElement - останній символ рядка.
+
+*/
+
+const courseTopic = "JavaScript essentials";
+const courseTopicLength = courseTopic.length;
+const firstElement = courseTopic[0];
+const lastElement = courseTopic[courseTopicLength - 1];
+
+// ПЕРЕВІРКА
+
+console.log (courseTopicLength);
+console.log (firstElement);
+console.log (lastElement);
+
+// Task 31-th - OK!
